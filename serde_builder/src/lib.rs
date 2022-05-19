8b433da8074543920a099695ff2b7e848c3a215c
Dan Spencer <danrspen@gmail.com>
2022-05-19 21:16:52 +0000

    More work in progress

--- serde_builder/src/lib.rs
+++ serde_builder/src/lib.rs
@@ -1,52 +1,61 @@
 use proc_macro::TokenStream;
+use quote::{format_ident, quote};
 use syn::{
     parse_macro_input, AttributeArgs, GenericArgument, ItemStruct, Lit, Meta, NestedMeta,
     PathArguments, PathSegment, ReturnType, Type,
 };
 
-#[proc_macro_attribute]
+#[proc_macro_derive(SerdeBuilder)]
 /// Generate a Serde builder for a struct
 /// ```
-/// # use serde_builder::serde_builder;
+/// # use serde_builder::SerdeBuilder;
 ///
-/// #[serde_builder]
+/// #[derive(SerdeBuilder)]
 /// struct Foo {
 ///    bar: String,
 /// }
 ///
 /// Foo::serde_builder();
-pub fn serde_builder(_args: TokenStream, item: TokenStream) -> TokenStream {
-    let raw_item = item.clone();
-    let item = parse_macro_input!(item as ItemStruct);
+pub fn derive_serde_builder(input: TokenStream) -> TokenStream {
+    let item = parse_macro_input!(input as ItemStruct);
 
-    let builder_name = format!("{}SerdeBuilder", item.ident);
+    let struct_name = item.ident.clone();
+    let builder_name = format_ident!("{}SerdeBuilder", item.ident);
 
-    format!(
-        r#"
-          {original_struct}
+    let fields = item
+        .fields
+        .into_iter()
+        .map(|f| quote! { #f.ident: Option<#f.ty> })
+        .collect::<Vec<_>>();
 
-          impl {struct_name} {{
-              pub fn serde_builder() -> {builder_name} {{
-                {builder_name}::new()
-              }}
-          }}
+    // println!(
+    //     "{}",
+    //     item.fields
+    //         .into_iter()
+    //         .map(|f| (f.ident, f.ty))
+    //         .collect::<Vec<_>>()
+    // );
 
-          struct {builder_name} {{
-              
-          }}
+    let expanded = quote! {
+        impl #struct_name {
+            pub fn serde_builder() -> #builder_name {
+                #builder_name::new()
+            }
+        }
 
-          impl {builder_name} {{
-            pub fn new() -> Self {{
-                Self {{
-                
-                }}
-            }}
-          }}
-    "#,
-        original_struct = raw_item,
-        struct_name = item.ident,
-        builder_name = builder_name,
-    )
-    .parse()
-    .expect("Generated invalid tokens")
+        // #[derive(serde::Serialize, serde::Deserialize)]
+        pub struct #builder_name {
+            #(#fields,)*
+        }
+
+        impl #builder_name {
+            pub fn new() -> Self {
+                Self {
+                }
+            }
+        }
+    };
+
+    print!("{}", expanded);
+    TokenStream::from(expanded)
 }
